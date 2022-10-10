e02fe5fc3965905b9e04b77e7c564a6b4052bb89
sechava4 <santiago.echavarria@optimizely.com>
2022-10-10 00:13:47 -0500

    Footer fixes and css comments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,27 +2,29 @@ import SocialMediaLogo from './SocialMediaLogo';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='social'>
-        <a href='#'>
-          <i className='fa fa-twitter'></i>
-        </a>
-        <a href='#'>
-          <i className='fa fa-facebook'></i>
-        </a>
-        <a href='#'>
-          <i className='fa fa-instagram'></i>
-        </a>
-        <a href='#'>
-          <i className='fa fa-github'></i>
-        </a>
-      </div>
+    <div className='footer-container'>
+      <footer className='footer'>
+        <div className='social'>
+          <a href='#'>
+            <i className='fa fa-twitter'></i>
+          </a>
+          <a href='#'>
+            <i className='fa fa-facebook'></i>
+          </a>
+          <a href='#'>
+            <i className='fa fa-instagram'></i>
+          </a>
+          <a href='#'>
+            <i className='fa fa-github'></i>
+          </a>
+        </div>
 
-      {/* <SocialMediaLogo fa_class={'fa fa-twitter'}></SocialMediaLogo>
+        {/* <SocialMediaLogo fa_class={'fa fa-twitter'}></SocialMediaLogo>
       <SocialMediaLogo fa_class={'fa fa-facebook'}></SocialMediaLogo>
       <SocialMediaLogo fa_class={'fa fa-instagram'}></SocialMediaLogo>
       <SocialMediaLogo fa_class={'fa fa-github'}></SocialMediaLogo> */}
-    </footer>
+      </footer>
+    </div>
   );
 };
 
